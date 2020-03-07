8be111671b6b2572d3dbbd95f94bbc0f68d1cf0f
vinayakusername <gpraut88@gmail.com>
2020-03-07 11:53:47 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Welcome to Flutter framework',
+              'Welcome to Flutter Framework',
             ),
           ],
           ),

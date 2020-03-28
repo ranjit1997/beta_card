b4b706a79a34ebc6de00b8fa20881c12ee94d689
vinayakusername <gpraut88@gmail.com>
2020-03-28 16:36:26 +0530

    Files Updated

--- lib/widget_ReorderableListView.dart
+++ lib/widget_ReorderableListView.dart
@@ -1,9 +1,11 @@
-import 'package:beta_card/widget_ListViewCard.dart';
+//import 'package:beta_card/widget_ListViewCard.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class ReorderableListView1 extends StatefulWidget {
-  ReorderableListView1({Key key, this.title}) : super(key: key);
 
+
+  ReorderableListView1({Key key, this.title}) : super(key: key);
   
   final String title;
 
@@ -16,6 +18,9 @@ class _ReorderableListViewState extends State<ReorderableListView1> {
   List<String> list1Items = [];
   final TextEditingController eCtrl = new TextEditingController();
   
+  
+
+
   @override
   Widget build(BuildContext context) 
   {
@@ -45,11 +50,13 @@ class _ReorderableListViewState extends State<ReorderableListView1> {
         children: List.generate(
           list1Items.length,
           (index) {
+            /*
             return ListViewCard(
               list1Items,
               index,
               Key('$index'),
-            );
+            );*/
+            return cardInfoWidget(index, Key('$index'), list1Items);
           },
         ),
       ),
@@ -60,7 +67,8 @@ class _ReorderableListViewState extends State<ReorderableListView1> {
     );
   }
 
-   void _onReorder(int oldIndex, int newIndex) {
+   void _onReorder(int oldIndex, int newIndex) 
+   {
     setState(
       () {
         if (newIndex > oldIndex) {
@@ -69,6 +77,66 @@ class _ReorderableListViewState extends State<ReorderableListView1> {
         final String item = list1Items.removeAt(oldIndex);
         list1Items.insert(newIndex, item);
       },
+    );
+  }
+
+  Widget cardInfoWidget( int index,  Key key, List<String> listItems)
+  {
+     
+  return Card(
+      key: key,
+      margin: EdgeInsets.all(4),
+      color: Colors.white,
+      child: InkWell(
+        splashColor: Colors.blue,
+        onTap: () => Fluttertoast.showToast(
+            msg: "Item ${listItems[index]} selected.",
+            toastLength: Toast.LENGTH_SHORT),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Flexible(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(8.0),
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Name: ${listItems[index]}',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                      textAlign: TextAlign.left,
+                      maxLines: 5,
+                    ),
+                  ),
+                  /*
+                  Container(
+                    padding: const EdgeInsets.all(8.0),
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      'Description ${widget.listItems[widget.index]}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal, fontSize: 16),
+                      textAlign: TextAlign.left,
+                      maxLines: 5,
+                    ),
+                  ),
+                  */
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+              child: Icon(
+                Icons.reorder,
+                color: Colors.grey,
+                size: 24.0,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

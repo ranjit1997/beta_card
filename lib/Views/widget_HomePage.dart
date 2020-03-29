4a7a21c43402d6907357e49153c495bcd7d8730a
vinayakusername <gpraut88@gmail.com>
2020-03-29 16:10:40 +0530

    Files Updated

--- lib/Views/widget_HomePage.dart
+++ lib/Views/widget_HomePage.dart
@@ -1,6 +1,8 @@
 
 import 'package:beta_card/Utility/DBWebService_FireBase_Authentication.dart';
 import 'package:beta_card/Views/widget_UploadPhoto.dart';
+import 'package:beta_card/model_UserInfo.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -21,6 +23,45 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+
+  List<UsersInfo> usersInfoList =[];
+
+  @override
+  void initState()
+  {
+    super.initState();
+
+    DatabaseReference usersInfoRef = FirebaseDatabase.instance.reference().child("UsersInfo");
+
+    usersInfoRef.once().then((DataSnapshot snap)
+    {
+         var KEYS = snap.value.keys;
+         var DATA = snap.value;
+
+         usersInfoList.clear();
+
+         for(var individualKeys in KEYS)
+         {
+           UsersInfo usersInfo = new UsersInfo
+           (
+             DATA[individualKeys]['FirstName'], 
+             DATA[individualKeys]['LastName'], 
+             DATA[individualKeys]['MobileNo'], 
+             DATA[individualKeys]['image'], 
+             DATA[individualKeys]['Date'], 
+             DATA[individualKeys]['Time']
+           );
+
+           usersInfoList.add(usersInfo);
+         }
+
+         setState(() 
+         {
+            print('Length: $usersInfoList.length');  
+         });
+    }); 
+  }
   
   void logOutUser() async
   {
@@ -47,7 +88,19 @@ class _MyHomePageState extends State<MyHomePage> {
                  backgroundColor: Colors.pink,
                ),
 
-       body: Container() ,
+       body: Container
+       (
+         child: usersInfoList.length==0?Text("No User Info is available"):ListView.builder
+         (
+           itemCount: usersInfoList.length,
+           itemBuilder: (_,index)
+           {
+             return userInfoWidget(usersInfoList[index].FirstName, usersInfoList[index].LastName, 
+             usersInfoList[index].MobileNo, usersInfoList[index].image, usersInfoList[index].Date, 
+             usersInfoList[index].Time);
+           }
+         ),
+       ) ,
 
        bottomNavigationBar: BottomAppBar
        (
@@ -99,7 +152,75 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-    
+    Widget userInfoWidget(String FirstName,String LastName,int MobileNo,String image,String Date,String Time)
+    {
+      return Card
+      (
+          elevation: 10.0,
+          margin: EdgeInsets.all(15.0),
+
+          child: Container
+          (
+            padding: EdgeInsets.all(15.0),
+
+            child: Column
+            (
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>
+              [
+                Row
+                (
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children:<Widget>
+                [
+                Text
+                (
+                  Date,
+                  style: Theme.of(context).textTheme.subtitle,
+                  textAlign: TextAlign.center,
+                ),
+
+                 Text
+                (
+                  Time,
+                  style: Theme.of(context).textTheme.subtitle,
+                  textAlign: TextAlign.center,
+                ),
+                ],
+                ),
+
+                SizedBox(height:10.0),
+                Image.network(image,fit:BoxFit.cover),
+                
+                SizedBox(height:10.0),
+
+                 Row
+                (
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children:<Widget>
+                [
+                Text
+                (
+                  FirstName,
+                  style: Theme.of(context).textTheme.subhead,
+                  textAlign: TextAlign.center,
+                ),
+
+                 Text
+                (
+                  LastName,
+                  style: Theme.of(context).textTheme.subhead,
+                  textAlign: TextAlign.center,
+                ),
+                ],
+                ),
+                
+                 
+              ],
+            ),
+          ),
+      );
+    }
 }
 
 
---
+++ lib/model_UserInfo.dart
@@ -0,0 +1,7 @@
+class UsersInfo
+{
+  String FirstName, LastName,Date, Time,image;
+  int MobileNo;
+
+  UsersInfo(this.FirstName,this.LastName,this.MobileNo,this.image,this.Date,this.Time);
+}
